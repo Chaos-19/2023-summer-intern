74b3bd8f1179d70add246e7e2cd4782e424a1d23
kalkidan getachew <kalgetachew375@gmail.com>
2024-08-08 15:54:42 +0300

    first commit

---
+++ src/app/components/forms/employ-form/employ-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employ-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './employ-form.component.html',
+  styles: ``
+})
+export class EmployFormComponent {
+
+}
